57b0e1980316d8cc06262f3a67d49fdabb2151ac
Danny Hadley <dadleyy@gmail.com>
2017-02-01 11:16:58 -0500

    committing bases.scss to bower.json file :hamburger:

--- app/pods/components/listing-table/component.js
+++ app/pods/components/listing-table/component.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
-import factory from 'charcoal/pods/components/hoc/table/component';
 
 const Row = Ember.Component.extend({
 });
 
-const Table = factory(Row);
-
-export default Table;
+export default Row;
