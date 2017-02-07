72ab21f650010dc26fa2d0940d4673cd4fdff2da
Danny Hadley <dadleyy@gmail.com>
2017-02-07 17:48:56 -0500

    adding i18n translation for loading text

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -1,14 +1,15 @@
 export default {
-  'about'        : 'About',
-  'home'         : 'Home',
-  'listings'     : 'Listings',
-  'listing_info' : 'Listing Info',
-  'listing_id'   : 'Listing ID',
-  'listing'      : 'Listing',
-  'loading'      : 'Loading',
-  'no_results'   : 'No Results',
-  'search'       : 'Search',
-  'shop'         : 'Shop',
-  'thumbnail'    : 'Thumbnail',
-  'title'        : 'Title'
+  'about'            : 'About',
+  'home'             : 'Home',
+  'listings'         : 'Listings',
+  'listing_info'     : 'Listing Info',
+  'listing_id'       : 'Listing ID',
+  'listing'          : 'Listing',
+  'loading'          : 'Loading',
+  'loading_listings' : 'Loading Listings, please wait...',
+  'no_results'       : 'No Results',
+  'search'           : 'Search',
+  'shop'             : 'Shop',
+  'thumbnail'        : 'Thumbnail',
+  'title'            : 'Title'
 };
